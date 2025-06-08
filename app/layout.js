1af3a4bed9bf46130148da3eff0acf0461a9f149
vulgar <vulgarlyf@gmail.com>
2025-06-08 10:46:43 +0000

    go

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Trackify| Expense Tracker and Management System",
+  title: "Trackify | Expense Tracker and Management System",
   description:
     "Trackify is a robust expense tracking and management tool designed to simplify financial oversight for individuals and businesses.",
 };
@@ -12,6 +12,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning={true}>
+      <head>
+        {/* ✅ Add PWA-related tags */}
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#000000" />
+      </head>
       <body className={`${inter.className} min-h-screen dark:bg-[#09090a]`}>
         {children}
       </body>
