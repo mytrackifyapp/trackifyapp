50b84d2bee1924e172b3d7c21b3ea4838ff669ef
rizzbrand <rizzbrandit@gmail.com>
2025-07-01 11:11:57 +0000

    newcommit

--- app/page.js
+++ app/page.js
@@ -244,7 +244,7 @@ export default function Home() {
           </motion.p>
           <motion.div className="mt-10 flex justify-center gap-x-6" variants={fadeIn}>
             <Link
-              href="/signup"
+              href={applicationClientUrls.auth.register}
               className="group inline-flex items-center justify-center rounded-full bg-gradient-to-r from-green-500 to-green-600 px-8 py-3 text-md font-semibold text-white hover:from-green-600 hover:to-green-700 focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600 active:bg-green-800 active:text-green-100 shadow-md hover:shadow-lg transition-all"
             >
               Get started for free
@@ -470,7 +470,7 @@ export default function Home() {
                 For professionals who need advanced financial tracking.
               </p>
               <div className="mt-6 flex items-baseline gap-x-1">
-                <span className="text-4xl font-bold tracking-tight text-gray-900">$15</span>
+                <span className="text-4xl font-bold tracking-tight text-gray-900">$2.99</span>
                 <span className="text-sm font-semibold leading-6 text-gray-600">/month</span>
               </div>
               <ul className="mt-8 space-y-3 text-sm leading-6 text-gray-600">
@@ -890,20 +890,22 @@ export default function Home() {
       <footer className="bg-white relative z-10">
         <div className="mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8">
           <div className="flex justify-center space-x-6 md:order-2">
-            <Link href="#" className="text-gray-400 hover:text-gray-500 transition-colors">
+            <Link href="https://x.com/TrackifyAI/" className="text-gray-400 hover:text-gray-500 transition-colors">
               <span className="sr-only">Twitter</span>
               <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                 <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
               </svg>
             </Link>
-            <Link href="#" className="text-gray-400 hover:text-gray-500 transition-colors">
-              <span className="sr-only">GitHub</span>
-              <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-                <path
-                  fillRule="evenodd"
-                  d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
-                  clipRule="evenodd"
-                />
+     
+            <Link href="https://www.instagram.com/mytrackifyapp/" className="text-gray-400 hover:text-gray-500 transition-colors">
+              <span className="sr-only">Instagram</span>
+              <svg
+                className="h-6 w-6"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+                aria-hidden="true"
+              >
+                <path d="M12 2.163c3.204 0 3.584.012 4.849.07 1.366.062 2.633.332 3.608 1.308.975.975 1.246 2.242 1.308 3.608.058 1.265.07 1.645.07 4.849s-.012 3.584-.07 4.849c-.062 1.366-.332 2.633-1.308 3.608-.975.975-2.242 1.246-3.608 1.308-1.265.058-1.645.07-4.849.07s-3.584-.012-4.849-.07c-1.366-.062-2.633-.332-3.608-1.308-.975-.975-1.246-2.242-1.308-3.608-.058-1.265-.07-1.645-.07-4.849s.012-3.584.07-4.849c.062-1.366.332-2.633 1.308-3.608.975-.975 2.242-1.246 3.608-1.308 1.265-.058 1.645-.07 4.849-.07zm0-2.163C8.741 0 8.332.014 7.052.072 5.773.13 4.68.392 3.757 1.315 2.834 2.238 2.572 3.331 2.514 4.61 2.456 5.89 2.442 6.299 2.442 12s.014 6.11.072 7.39c.058 1.279.32 2.372 1.243 3.295.923.923 2.016 1.185 3.295 1.243 1.279.058 1.688.072 7.39.072s6.11-.014 7.39-.072c1.279-.058 2.372-.32 3.295-1.243.923-.923 1.185-2.016 1.243-3.295.058-1.279.072-1.688.072-7.39s-.014-6.11-.072-7.39c-.058-1.279-.32-2.372-1.243-3.295-.923-.923-2.016-1.185-3.295-1.243C15.668.014 15.259 0 12 0zm0 5.838a6.162 6.162 0 100 12.324 6.162 6.162 0 000-12.324zm0 10.162a3.999 3.999 0 110-8 3.999 3.999 0 010 8zm6.406-11.845a1.44 1.44 0 100-2.88 1.44 1.44 0 000 2.88z" />
               </svg>
             </Link>
           </div>
--- components/constant/urls.js
+++ components/constant/urls.js
@@ -8,7 +8,7 @@ export const usagePlan = {
   premiumPlan: { limit: 1000, name: "premium" },
 };
 
-const domain = "trackifyapp.onrender.com";
+const domain = "mytrackify.com";
 const local = "localhost:3000"; // No protocol
 const rawHost = isProduction ? domain : local;
 const hostUrl = `${isProduction ? "https://" : "http://" }${rawHost}`;
